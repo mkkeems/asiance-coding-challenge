bfd108f86cdaa3476d09172cd06f109419fe2337
Michelle Kim <mkkeems@gmail.com>
2020-06-26 02:36:37 +0900

    set up routes for api end points

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :authors
+      resources :posts
+    end
+  end
 end
